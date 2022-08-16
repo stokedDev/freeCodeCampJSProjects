82eb8217f46a36e83f309bae4a000004915131e6
mauriceRogers <scsurfermaurice@gmail.com>
2022-08-15 18:43:26 -0700

    Refactor palindrome checker JS
    Change let variables to const and remove regex variable in favor of direct regex placed in replace method as it's first argument

--- palindromeChecker.js
+++ palindromeChecker.js
@@ -1,7 +1,6 @@
 function palindrome(str) {
-    let regex = /\s+|\W+|_/g
-    let oneword = [...str.replace(regex, '').toLowerCase()];
-    return oneword.join('') === oneword.reverse().join('')
+    const oneword = [...str.replace(/\s+|\W+|_/g, '').toLowerCase()];
+    return oneword.join('') === oneword.reverse().join('');
   }
   
-  palindrome("0_0 (: /-\ :) 0-0")
+  palindrome("0_0 (: /-\ :) 0-0");
